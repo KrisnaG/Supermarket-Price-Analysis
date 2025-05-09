102e34b111304514fb03e9698749cb7d84b25664
krisnag <k_gusti@hotmail.com>
2025-05-09 11:37:06 +0000

    Added graph and add product button. Corrected table.

--- src/app/product_tracker_app.py
+++ src/app/product_tracker_app.py
@@ -1,4 +1,7 @@
+import pandas as pd
 import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import ttk, messagebox
 from tktooltip import ToolTip
 
@@ -8,7 +11,9 @@ from src.tools.csv_tools import save_products_to_csv
 
 
 class ProductTrackerApp(tk.Tk):
-    def __init__(self, size: tuple = (1200, 900)):
+    _number_of_main_components = 6
+
+    def __init__(self, size: tuple = (2000, 1200)):
         super().__init__()
         self.title("Product Price Tracker")
         self.geometry(f"{size[0]}x{size[1]}")
@@ -31,13 +36,17 @@ class ProductTrackerApp(tk.Tk):
         update_product_button.pack(side="left", padx=5)
         ToolTip(update_product_button, msg="Update products from all stores", x_offset=25, y_offset=25)
 
-        show_price_history_button = ttk.Button(btn_frame, text="Show Price History", command=self.show_price_graph)
+        show_price_history_button = ttk.Button(btn_frame, text="Price History", command=self.show_price_graph)
         show_price_history_button.pack(side="left", padx=5)
         ToolTip(show_price_history_button, msg="Show price history of products", x_offset=25, y_offset=25)
 
-        show_product_table_button = ttk.Button(btn_frame, text="Show Product Table", command=self.show_product_table)
+        show_product_table_button = ttk.Button(btn_frame, text="Product Table", command=self.show_product_table)
         show_product_table_button.pack(side="left", padx=5)
         ToolTip(show_product_table_button, msg="Show all products in a table", x_offset=25, y_offset=25)
+
+        add_product_button = ttk.Button(btn_frame, text="Add Product", command=self.add_new_product)
+        add_product_button.pack(side="left", padx=5)
+        ToolTip(add_product_button, msg="Add a new product to the Database", x_offset=25, y_offset=25)
 
         download_csv_button = ttk.Button(btn_frame, text="Download CSV", command=self.download_all_products_to_csv)
         download_csv_button.pack(side="left", padx=5)
@@ -92,19 +101,67 @@ class ProductTrackerApp(tk.Tk):
             messagebox.showinfo("Success", message)
 
     def show_price_graph(self):
-        pass
+        """Show a graph of product prices over time."""
+        self.destroy_non_main_components()
+
+        all_products = self.product_repository.get_all_products()
+        data = {
+            "date": [product.date for product in all_products],
+            "product_name": [product.product_name for product in all_products],
+            "price": [product.price for product in all_products],
+        }
+        df = pd.DataFrame(data)
+        pivot = df.pivot(index='date', columns='product_name', values='price')
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        pivot.plot(ax=ax, title="Product Price History", marker='o')
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Price")
+        ax.grid(True)
+        fig.autofmt_xdate()
+        plot_frame = ttk.Frame(self)
+        plot_frame.pack(fill='both', pady=10, expand=True)
+
+        canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+        canvas.get_tk_widget().pack(fill="both", expand=True)
 
     def show_product_table(self):
+        """Show a table of all products in the database."""
+        self.destroy_non_main_components()
+
         all_products = self.product_repository.get_all_products()
 
         style = ttk.Style()
         style.configure("Treeview", rowheight=40)
+        style.configure("Treeview.Heading", anchor="center")
 
-        table = ttk.Treeview(self, columns=("Date", "Stockcode", "Store", "Price"), show="headings")
-        table.heading("Date", text="Date")
-        table.heading("Stockcode", text="Stockcode")
-        table.heading("Store", text="Store")
-        table.heading("Price", text="Price")
+        # Create a frame to hold the table and scrollbars
+        frame = ttk.Frame(self)
+        frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+        # Create scrollbars
+        v_scrollbar = ttk.Scrollbar(frame, orient="vertical")
+        h_scrollbar = ttk.Scrollbar(frame, orient="horizontal")
+
+        # Create the table with scrollbar configurations
+        table = ttk.Treeview(frame, columns=("Date", "ProductName", "Price", "Store"), show="headings",
+                             yscrollcommand=v_scrollbar.set, xscrollcommand=h_scrollbar.set)
+
+        # Configure scrollbars
+        v_scrollbar.config(command=table.yview)
+        h_scrollbar.config(command=table.xview)
+
+        # Configure column widths and alignments
+        column_widths = {
+            "Date": 100,
+            "ProductName": 400,
+            "Price": 100,
+            "Store": 150
+        }
+
+        for column in ("Date", "ProductName", "Price", "Store"):
+            table.heading(column, text=column, command=lambda _col=column: sort_table(_col, False))
+            table.column(column, width=column_widths[column], anchor="center")
 
         def sort_table(col, reverse):
             """Sort the table by the given column."""
@@ -114,10 +171,46 @@ class ProductTrackerApp(tk.Tk):
                 table.move(child, "", index)
             table.heading(col, command=lambda: sort_table(col, not reverse))
 
-        for column in ("Date", "Stockcode", "Store", "Price"):
-            table.heading(column, text=column, command=lambda _col=column: sort_table(_col, False))
-
-        table.pack(fill="both", expand=True)
+        # Pack scrollbars and table
+        v_scrollbar.pack(side="right", fill="y")
+        h_scrollbar.pack(side="bottom", fill="x")
+        table.pack(side="left", fill="both", expand=True)
 
         for product in all_products:
-            table.insert("", "end", values=(product.date, product.stockcode, product.store, product.price))
+            table.insert("", "end", values=(product.date, product.product_name, product.price,
+                                            product.store))
+
+    def add_new_product(self):
+        """Open a new window to add a new product."""
+        self.destroy_non_main_components()
+        # Create a new window
+        add_product_frame = ttk.Frame(self)
+        add_product_frame.pack(pady=10)
+        add_product_label = ttk.Label(add_product_frame, text="Add a new product")
+        add_product_label.pack()
+        # Create entry fields for stockcode
+        stockcode_label = ttk.Label(add_product_frame, text="Stockcode:")
+        stockcode_label.pack()
+        stockcode_entry = ttk.Entry(add_product_frame)
+        stockcode_entry.pack()
+        stockcode_entry.focus()
+        # Create entry field for store
+        store_label = ttk.Label(add_product_frame, text="Store:")
+        store_label.pack()
+        store_entry = ttk.Entry(add_product_frame)
+        store_entry.pack()
+        store_entry.focus()
+        # Create a button to add the product
+        add_product_button = ttk.Button(add_product_frame, text="Add Product",
+                                        command=lambda: self.add_product(stockcode_entry.get(), store_entry.get()))
+        add_product_button.pack(pady=10)
+
+    def add_product(self, stockcode: str, store: str):
+        """Add a new product to the database."""
+        # TODO
+        pass
+
+    def destroy_non_main_components(self):
+        """Destroy all components that are not the main components."""
+        for widget in self.winfo_children()[self._number_of_main_components:]:
+            widget.destroy()
